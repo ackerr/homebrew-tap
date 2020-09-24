0f338beb81aaf108451753e978ff79d35be55acd
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-24 22:00:28 +0800

    Brew formula update for lab version v0.0.9

--- lab.rb
+++ lab.rb
@@ -2,16 +2,16 @@
 class Lab < Formula
   desc "A cli tool with gitlab."
   homepage "https://github.com/Ackerr/lab"
-  version "0.0.8"
+  version "0.0.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Ackerr/lab/releases/download/v0.0.8/lab_Darwin_x86_64.tar.gz"
-    sha256 "57128da805c8aeb1d2b14d526518fbf5dbf19b5d5e1bff8ceca833c70bcb8c60"
+    url "https://github.com/Ackerr/lab/releases/download/v0.0.9/lab_Darwin_x86_64.tar.gz"
+    sha256 "33f5a666056092784cb1153824bff38ccc5a5c7221a2ae1a8a2f60170d8f5eb5"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Ackerr/lab/releases/download/v0.0.8/lab_Linux_x86_64.tar.gz"
-      sha256 "227a5b6244c0e7794512bc1181983879903f689cdc383b03326f39cc9dfa6e0c"
+      url "https://github.com/Ackerr/lab/releases/download/v0.0.9/lab_Linux_x86_64.tar.gz"
+      sha256 "40419b90285682a1f472893698738bb57cdfb5d2156124b011ebad7cdf9fbb21"
     end
   end
 
